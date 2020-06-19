7289cf9a6b2a2bf520c764a83f3e757fbf904ad8
vhugo <victor.hugo@8x8.com>
2020-06-19 15:40:04 -0300

    Adding Feedback page

--- src/Navegation.js
+++ src/Navegation.js
@@ -13,6 +13,7 @@ export default function Navegation(){
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/createKudos">Give a kudo</Nav.Link>
+            <Nav.Link href="https://docs.google.com/forms/d/1w3yBJwgJNfDJme0cprs1NMd8puLrEqZEKo-_2dPITgA/edit?ts=5eed000c">Give a Feedback</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
