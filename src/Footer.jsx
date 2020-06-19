d89c24bf619eb576e50ebe62297575272c4ebfdb
vhugo <victor.hugo@8x8.com>
2020-06-19 17:10:51 -0300

    Added footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {MDBContainer, MDBFooter } from "mdbreact";
+import {MDBContainer, MDBFooter,MDBRow,MDBCol} from "mdbreact";
 import './Footer.css';
 
 class Footer extends Component {
@@ -7,13 +7,10 @@ class Footer extends Component {
     {
         return (
             <MDBFooter color="blue" className="font-small pt-4 mt-4">
-            <div className="footer-copyright text-center py-3">
-                <MDBContainer fluid>
-                    <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-                    &copy; {new Date().getFullYear()} Copyright: <a href="https://www.daitan.com/"> Daitan Group </a>
-                </MDBContainer>
-            </div>
-            </MDBFooter>
+                  <div className="iconsRef">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                    <div className="copyright">&copy; {new Date().getFullYear()} Copyright: <a href="https://www.daitan.com/"> Daitan Group </a></div>
+                    <div className="feedback"><a href="https://forms.gle/8nN5u8vGsJJpBJbw6"> Give us a feedback </a></div>
+          </MDBFooter>
         );
     }
 }
