733e4758fe984fadf6726caab653db1a610dbd9f
vhugo <victor.hugo@8x8.com>
2020-06-24 10:41:02 -0300

    Adding Dockerfile

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,7 +8,7 @@ class Footer extends Component {
         return (
             <MDBFooter color="blue" className="font-small pt-4 mt-4">
                   <div className="iconsRef">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-                    <div className="copyright">&copy; {new Date().getFullYear()} Copyright: <a href="https://www.daitan.com/"> Daitan Group </a></div>
+                    <div className="copyright"></div>
                     <div className="feedback"><a href="https://forms.gle/8nN5u8vGsJJpBJbw6"> Give us a feedback </a></div>
           </MDBFooter>
         );
