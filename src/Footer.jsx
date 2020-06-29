7bb34141481570af1386629fecd79fe752b8182b
vhugo <victor.hugo@8x8.com>
2020-06-29 16:47:45 -0300

    Removing daitan reference

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,8 +7,8 @@ class Footer extends Component {
     {
         return (
             <MDBFooter color="blue" className="font-small pt-4 mt-4">
-                  <div className="iconsRef">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-                    <div className="copyright"></div>
+                    <div className="iconsRef">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                   
                     <div className="feedback"><a href="https://forms.gle/8nN5u8vGsJJpBJbw6"> Give us a feedback </a></div>
           </MDBFooter>
         );
