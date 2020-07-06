a0f036c09aeb1e37bb3ee5f024631b093d308a2c
vhugo <victor.hugo@8x8.com>
2020-07-06 17:32:26 -0300

    Changing user form and its fields

--- src/View/User/CreateUser/UserForm.jsx
+++ src/View/User/CreateUser/UserForm.jsx
@@ -4,81 +4,135 @@ import './UserForm.css';
 
 class UserForm extends Component{
 
-    state = this.props.user;
+    INICIAL_STATE = {
+        welcomeMsg: "Create new user",
+        user: this.props.user,
+        newTag:""
+    };
+
+    constructor (props) {
+        super(props);
+        this.state = this.INICIAL_STATE;
+    }
 
     isValid = () => {
+        console.log("validating",this.state.user)
+        if (!this.state.user.name) {
 
-        this.setState({error: ""});
-        
-        if (!this.state.name) {
-            this.setState({error: "Name field cannot be blank!"});
+            this.setState((prevState) => ({
+                user: {
+                    ...prevState.user,
+                    error: "Name field can't be empty!",
+                }
+            }));
             return false;
         }
         
-        if (!this.state.user) {
-            this.setState({error: "User field cannot be blank!"});
-            return false;
-        }
-        
-        if (!this.state.email) {
-            this.setState({error: "Email field cannot be blank!"});
-            return false;
-        }
-        
-        if (!this.state.email.includes('@')) {//email deve ter um @
-            this.setState({error: "Email must include @"});
-            return false; 
-        }
+        if (!this.state.user.email) {
 
-        if (!this.state.password) {
-            this.setState({error: "Password field cannot be blank!"});
+            this.setState((prevState) => ({
+                user: {
+                    ...prevState.user,
+                    error: "Email field can't be empty!",
+                }
+            }));
             return false;
         }
         
-        if (this.state.password.length < 4) {//senha deve ter pelo menos 4 caracteres
-            this.setState({error: "Password length can't be shorter than 4 caracters"});
+        if (!this.state.user.password) {
+
+            this.setState((prevState) => ({
+                user: {
+                    ...prevState.user,
+                    error: "Password field can't be empty!",
+                }
+            }));
             return false;
         }
-
+        
         return true;
-        
     }
+
+    updateName = (value) => {
+
+        this.setState((prevState) => ({
+            user: {
+                ...prevState.user,
+                name: value,
+            }
+        }));
+    };
+
+    updateEmail = (value) => {
+
+        this.setState((prevState) => ({
+            user: {
+                ...prevState.user,
+                email: value,
+            }
+        }));
+    };
+
+    updatePassword = (value) => {
+
+        this.setState((prevState) => ({
+            user: {
+                ...prevState.user,
+                password: value,
+            }
+        }));
+    };
+
+    updateTags = (value) => {
+
+        this.setState(() => ({
+                newTag: value,
+        }));
+    };
+
+    addTag = (event) => {
+        event.preventDefault();
+       
+        this.setState((prevState) => ({
+            user: {
+                ...prevState.user,
+                tags: prevState.user.tags.concat(prevState.newTag),
+            },
+            newTag : "",
+        }));
+
+    };
 
     onSubmit = (event) => {
         event.preventDefault();
         if(this.isValid())
         {
-           this.props.createUser(this.state);
-           this.setState(this.props.user);
+           this.props.createUser(this.state.user);
+           this.setState(this.INICIAL_STATE);
         }
     };
 
     render()
     {
-       
         return(
-
             <div>
                 <form method="post" noValidate>
                     <div className="login-div">
-                        <div className="title">MagicKudos</div>
-                        <div className="sub-title">Version 1.0</div>
-                        
+                        <h5 className="form-welcome-msg">{this.state.welcomeMsg}</h5>
                         <div className="fields">
-                            <input id="inputName" placeholder="name" type="text" value={this.state.name} required onChange={event => this.setState({name: event.target.value})}/><br/>
-                            <input id="inputUser" placeholder="user" type="text" value={this.state.user} required onChange={event => this.setState({user: event.target.value})} /><br/>
-                            <input id="inputEmail" placeholder="email" type="text" value={this.state.email} required onChange={event => this.setState({email: event.target.value})} /><br/>
-                            <input id="inputPassword" placeholder="password" type="password" value={this.state.password} required onChange={event => this.setState({password: event.target.value})} /><br/>
-                        
-                            <br/><button className="createButton" onClick={this.onSubmit}>Create User</button>
-                            <h5 className="inputError">{this.state.error}</h5>
+                            <input id="inputName" placeholder="name" type="text" value={this.state.user.name} required onChange={event => this.updateName(event.target.value)}/>
+                            <input id="inputEmail" placeholder="email" type="text" value={this.state.user.email} required onChange={event => this.updateEmail(event.target.value)}/>
+                            <input id="inputPassword" placeholder="password" type="password" value={this.state.user.password} required onChange={event => this.updatePassword(event.target.value)}/>
+                            <input id="inputTag" placeholder="Add a tag" type="text" value={this.state.newTag} required onChange={event => this.updateTags(event.target.value)}/>
+                            <button className="tagButton" onClick={this.addTag}>Add new tag</button>
+                            <button className="createButton" onClick={this.onSubmit}>Create User</button>
+                            <h5 className="allTags">{this.state.user.tags}</h5>
+                            <h5 className="inputError">{this.state.user.error}</h5>
                         </div>
                         
                     </div>
                 </form>
-            </div>
-         
-            
+            </div> 
         );
     }
 }
