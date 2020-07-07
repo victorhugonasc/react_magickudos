7ed3b48e92542bc76fcd7aa88d802e3fc1c9a304
vhugo <victor.hugo@8x8.com>
2020-07-07 14:46:19 -0300

    User form with css working

--- src/View/User/CreateUser/UserForm.jsx
+++ src/View/User/CreateUser/UserForm.jsx
@@ -7,7 +7,8 @@ class UserForm extends Component{
     INICIAL_STATE = {
         welcomeMsg: "Create new user",
         user: this.props.user,
-        newTag:""
+        newTag:"",
+        getTags:"",
     };
 
     constructor (props) {
@@ -16,7 +17,6 @@ class UserForm extends Component{
     }
 
     isValid = () => {
-        console.log("validating",this.state.user)
         if (!this.state.user.name) {
 
             this.setState((prevState) => ({
@@ -90,18 +90,88 @@ class UserForm extends Component{
         }));
     };
 
+
     addTag = (event) => {
         event.preventDefault();
-       
-        this.setState((prevState) => ({
-            user: {
-                ...prevState.user,
-                tags: prevState.user.tags.concat(prevState.newTag),
-            },
-            newTag : "",
-        }));
 
+        if(this.isTagValid()){
+
+            console.log(this.state.user.tags);
+
+            if (this.state.user.tags.length !== 0)
+            {
+                this.setState((prevState) => ({
+                    user: {
+                        ...prevState.user,
+                        tags: prevState.user.tags.concat(prevState.newTag),
+                    },
+                    getTags: prevState.getTags.concat(",").concat(prevState.newTag),
+                    newTag : "",
+                }));
+            }
+
+            else{
+                this.setState((prevState) => ({
+                    user: {
+                        ...prevState.user,
+                        tags: prevState.user.tags.concat(prevState.newTag),
+                    },
+                    getTags: prevState.getTags.concat(prevState.newTag),
+                    newTag : "",
+                }));
+            }
+           
+        }
+            
     };
+
+    isTagValid = () => {
+
+        const tags = this.state.user.tags;
+
+        if (!this.state.newTag) {
+
+            this.setState((prevState) => ({
+                user: {
+                    ...prevState.user,
+                    error: "Tag field can't be empty!",
+                }
+            }));
+            return false;
+        }
+
+        else{
+
+            for( var i = 0; i < tags.length; i++){
+                if(tags[i] === this.state.newTag)
+                {
+                    this.setState((prevState) => ({
+                        user: {
+                            ...prevState.user,
+                            error: "Tag already exists!",
+                        }
+                    }));
+                    return false;
+                }
+            }
+
+            this.setState((prevState) => ({
+                user: {
+                    ...prevState.user,
+                    error: "",
+                }
+            }));
+            return true;
+        }
+    }
+
+
+    renderRow(tag) {
+       if(this.state.newTag !== tag){
+           return true;
+       }
+       return false;
+    }
 
     onSubmit = (event) => {
         event.preventDefault();
@@ -117,16 +187,18 @@ class UserForm extends Component{
         return(
             <div>
                 <form method="post" noValidate>
-                    <div className="login-div">
+                    <div className="login-default">
                         <h5 className="form-welcome-msg">{this.state.welcomeMsg}</h5>
                         <div className="fields">
                             <input id="inputName" placeholder="name" type="text" value={this.state.user.name} required onChange={event => this.updateName(event.target.value)}/>
                             <input id="inputEmail" placeholder="email" type="text" value={this.state.user.email} required onChange={event => this.updateEmail(event.target.value)}/>
                             <input id="inputPassword" placeholder="password" type="password" value={this.state.user.password} required onChange={event => this.updatePassword(event.target.value)}/>
-                            <input id="inputTag" placeholder="Add a tag" type="text" value={this.state.newTag} required onChange={event => this.updateTags(event.target.value)}/>
-                            <button className="tagButton" onClick={this.addTag}>Add new tag</button>
-                            <button className="createButton" onClick={this.onSubmit}>Create User</button>
-                            <h5 className="allTags">{this.state.user.tags}</h5>
+                            <div className="divTag">
+                                <input id="inputTag" placeholder="Add a tag" type="text" value={this.state.newTag} required onChange={event => this.updateTags(event.target.value)}/>
+                                <button className="tagButton" onClick={this.addTag}>+</button>
+                                <h5 className="allTags">{this.state.getTags}</h5>
+                            </div>
+                            <button className="createButton" onClick={this.onSubmit}>Create!</button>
                             <h5 className="inputError">{this.state.user.error}</h5>
                         </div>
                         
