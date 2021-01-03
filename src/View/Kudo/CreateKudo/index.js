c117ee1fc9bc2dccc4ab46406f6e4405a47aef50
donascvh <victorhugo.donascimento@bt.com>
2021-01-03 01:19:05 -0300

    New look to KudoForm page

--- src/View/Kudo/CreateKudo/index.js
+++ src/View/Kudo/CreateKudo/index.js
@@ -1,18 +1,21 @@
 
-import {createKudo} from '../../../State/Kudo/CreateKudoActions';
-import {connect} from 'react-redux';
+import { createKudo } from '../../../State/Kudo/CreateKudoActions';
+import { connect } from 'react-redux';
+import { fetchColorPallete } from '../../../State/Color/GetColors/GetColorPalleteActions';
 import KudoForm from './KudoForm';
 
 function mapStateToProps(state) {
    
     return {
-        kudo: state.create.kudo
+        kudo: state.create.kudo,
+        pallete: state.getColorReducer.pallete
     }
 }
 
 function mapDispatchToProps(dispatch) {
     return {
-        createKudo: kudo => dispatch(createKudo(kudo))
+        createKudo: kudo => dispatch(createKudo(kudo)),
+        fetchColorPallete: () => dispatch(fetchColorPallete())
     }
 }
 
